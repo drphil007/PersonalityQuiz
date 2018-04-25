5f96a524a9f5778ee4383cee65dd9d3c3316248d
Philine Wairata <philinewairata@wcw-stud-145-109-2-39.wireless.uva.nl>
2018-04-25 12:46:45 +0200

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Philine Wairata on 25/04/2018.
+//  Copyright © 2018 Philine Wairata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
